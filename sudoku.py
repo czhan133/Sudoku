0175c7db72441c7be33a0b62e26af931cf5f9a6d
Caitlyn Zhang <czhan133@sledge.cs.ucr.edu>
2020-06-12 20:20:03 -0700

    update

--- sudoku.py
+++ sudoku.py
@@ -37,8 +37,7 @@ def find_empty(g):
 	return -1, -1
 
 def possible_answers(g, c, r):
-	#c, r = find_empty(g)
-
+	
 	possible = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 	
 	#searching all columns at index r
@@ -53,30 +52,51 @@ def possible_answers(g, c, r):
 
 	return possible
 
-def attempt(g, c, r):
-	#column, row = find_empty(g)
-
-	p = possible_answers(grid, c, r)
-
-	for i in range(9):
-		if (p[i] != 0):
-			grid[c][r] = p[i];
-			break;
-
 def solve(g):
 	column, row = find_empty(g)
-	backtrack = [[column, row]] 
+
+	entry = [column, row]
+	backtrack = [entry]
+
+	p_arr = possible_answers(grid, column, row)
+	b_poss = [p_arr]
 
 	while(column != -1 and row != -1):
-		attempt(grid, column, row)
-		column, row = find_empty(g)
-		entry = [column, row]
-		backtrack.insert(0,entry)
+		i = 0
+
+		while (i < 9):
+			if (b_poss[0][i] != 0):
+				grid[column][row] = b_poss[0][i];
+				b_poss[0][i] = 0;
+				break;
+
+			if (i == 8):
+				grid[column][row] = 0;
+
+			i += 1
+
+		#print_puzzle(grid)
+
+
+		if (grid[column][row] == 0):
+			#means something went wrong, we need to backtrack
+			backtrack.pop(0)
+			column = backtrack[0][0]
+			row = backtrack[0][1]
+			b_poss.pop(0)
+
+		else:	
+			column, row = find_empty(g)
+			entry = [column, row]
+			backtrack.insert(0, entry)
+			p_arr = possible_answers(grid, column, row)
+			b_poss.insert(0, p_arr)
+
 
 #Declarations#
 print("Sudoku puzzle: ")
 print_puzzle(grid)
 solve(grid)
-print("Attempt 1: ")
+print("Solution: ")
 print_puzzle(grid)
 
