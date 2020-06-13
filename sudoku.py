c5e4c229c79715af9e37b8dcb8fbb27325f6595a
Caitlyn Zhang <czhan133@sledge.cs.ucr.edu>
2020-06-12 21:12:09 -0700

    update

--- sudoku.py
+++ sudoku.py
@@ -11,12 +11,12 @@ grid = [
 ]
 
 def print_puzzle(g):
-        print("----------------------------")
-	for i in range(9):
+	print("-"*28);
+	for i in range(len(grid[0])):
 		if (i % 3 == 0 and i != 0):
-			print("----------------------------")
-	
-		for j in range(9):
+			print("-"*28);
+
+		for j in range(len(grid[0])):
 			if (j % 3 == 0):
 				print("| "),
 
@@ -26,29 +26,44 @@ def print_puzzle(g):
 				print(g[i][j]),
 		
 		print("|")
-	print("----------------------------")         
+	print("-"*28);
 
 def find_empty(g):
-	for i in range(9):
-		for j in range(9):
+	for i in range(len(grid[0])):
+		for j in range(len(grid[0])):
 			if (g[i][j] == 0):
 				return (i, j)
 
 	return -1, -1
 
 def possible_answers(g, c, r):
-	
-	possible = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-	
+	possible = []
+
+	for i in range(len(grid[0])):
+		possible.append(i + 1);
+
 	#searching all columns at index r
-	for i in range(9):
+	for i in range(len(grid[0])):
 		if (g[i][r] != 0):
 			possible[g[i][r] - 1] = 0
 	
 	#searching row c at all indices
-	for j in range(9):
+	for j in range(len(grid[0])):
 		if (g[c][j] != 0):
 			possible[g[c][j] - 1] = 0
+
+	start_c = c / 3;
+	start_r = r / 3;
+
+	start_c = start_c * 3;
+	start_r = start_r * 3;
+
+	spacing = len(grid[0]) / 3;
+
+	for a in range(spacing):
+		for b in range(spacing):
+			if (g[a + start_c][b + start_r] != 0):
+				possible[g[a + start_c][b + start_r] - 1] = 0			
 
 	return possible
 
@@ -64,19 +79,16 @@ def solve(g):
 	while(column != -1 and row != -1):
 		i = 0
 
-		while (i < 9):
+		while (i < len(grid[0])):
 			if (b_poss[0][i] != 0):
 				grid[column][row] = b_poss[0][i];
 				b_poss[0][i] = 0;
 				break;
 
-			if (i == 8):
+			if (i == (len(grid[0]) - 1)):
 				grid[column][row] = 0;
 
 			i += 1
-
-		#print_puzzle(grid)
-
 
 		if (grid[column][row] == 0):
 			#means something went wrong, we need to backtrack
